9e0f5fdb8c570f4e10d26ce4379d95bbe9e9fa3f
Gabriel C Silva <46769816+gbri-dev@users.noreply.github.com>
2024-01-14 00:00:14 -0300

    feat: filtro de tarefas

--- TodoList_VUE/src/App.vue
+++ TodoList_VUE/src/App.vue
@@ -1,30 +1,95 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+
+const estadoApp = reactive({
+  tarefaTemp: '',
+  filtro: 'todas',
+  tarefas: [
+    {
+      titulo: 'Estudar ES6',
+      concluido: false
+    },
+    {
+      titulo: 'Estudar SASS',
+      concluido: false
+    },
+    {
+      titulo: 'Fazer caminhada e corrida',
+      concluido: true
+    }
+  ]
+})
+
+const getTarefasPendentes = () => {
+  return estadoApp.tarefas.filter(tarefa => !tarefa.concluido)
+}
+
+const getTarefasConcluidas = () => {
+  return estadoApp.tarefas.filter(tarefa => tarefa.concluido)
+}
+
+const getTarefasFiltradas = () => {
+  const { filtro } = estadoApp
+
+  switch(filtro){
+    case 'pendentes':
+      return getTarefasPendentes();
+    case 'concluidas':
+      return getTarefasConcluidas();
+      default: 
+      return estadoApp.tarefas;
+  }
+}
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estadoApp.tarefaTemp,
+    finalizada: false
+  }
+  estadoApp.tarefas.push(tarefaNova)
+  estadoApp.tarefaTemp = ''
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>
+        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
+      </p>
+    </header>
+
+  <form @submit.prevent="cadastraTarefa">
+    <div class="row">
+      <div class="col-sm mb-4">
+        <input @value="evento.tarefaTemp" @change="evento => evento.tarefaTemp = evento.target.value" type="text" class="form-control" placeholder="Digite sua tarefa aqui...">
+      </div>
+      <div class="col-sm-1 mb-4">
+        <button  type="submit" class="btn btn-primary">Cadastrar</button>
+      </div>
+      <div class="col-sm-2">
+        <select class="form-control" @change="evento => estadoApp.filtro = evento.target.value">
+          <option value="todas">Todas tarefas</option>
+          <option value="pendentes">Pendentes</option>
+          <option value="concluidas">Concluídas</option>
+        </select>
+      </div>
+    </div>
+  </form>
+  <ul class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+      <input type="checkbox" :checked="tarefa.concluido" :id="tarefa.titulo" @change="evento => tarefa.concluido = evento.target.checked">
+      <label :for="tarefa.titulo" class="ms-3" :class="{ done: tarefa.concluido}">
+        {{ tarefa.titulo }}
+      </label>
+    </li>
+  </ul>
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.done {
+  text-decoration: line-through;
 }
 </style>
